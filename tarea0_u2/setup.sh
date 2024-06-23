7766957db5077d9571e06cfed4e8e0dcfb1b695c
Brandon Arroyo <brandon.arroyo.2000@gmail.com>
2024-06-23 03:39:19 -0600

    Arreglando falla en setup.sh

--- tarea0_u2/setup.sh
+++ tarea0_u2/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 apt-get update
-apt-get install -y hugo make
+apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.127.0/hugo_0.127.0_linux-amd64.deb
+dpkg -i hugo_0.127.0_linux-amd64.deb
+rm hugo_0.127.0_linux-amd64.deb
 make build
